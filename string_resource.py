131fc6809c18742023dfd133d6b2cd980cc53fd0
stefanavra <stefan.avra@gmail.com>
2019-04-03 16:12:19 +0200

    random order of facts is generated when initializing game

--- string_resource.py
+++ string_resource.py
@@ -1,5 +1,6 @@
 import random
 
+
 __strings = {
     'lost_life': 'YOU LOST A CIG!\nPRESS START TO LIGHT UP\nANOTHER ONE',
     'stage_text': 'STAGE: {}',
@@ -21,16 +22,27 @@ __facts = {
     5: 'At least four of the actors who played the Marlboro Man have died of smoking-related diseases.',
     6: '67% of Indonesia\'s male popularity smokes.',
     7: 'The Marlboro Man is still used in Japan, where smoking is widespread in the male population.',
-    8: 'Philip Morris (the company that sells Marlboro) is known to sponsor legal costs in lawsuits, if a country happens to sue another country over anti-smoking laws.'
+    8: 'Philip Morris (the company that sells Marlboro) is known to sponsor legal costs in lawsuits, if a country'
+       ' happens to sue another country over anti-smoking laws.'
 }
+
+fact_order = [i for i in range(len(__facts) - 1)]
+random.shuffle(fact_order)
+current_fact = 0
 
 
 def get_string(name):
     return __strings.get(name)
 
 
-def get_fact(number=99):
-    if number == 99:
-        number = random.randint(0, len(__facts)-1)
-    return __facts.get(number).upper()
+def get_fact(number=None):
+    if number is None:
+        global current_fact
+        number = current_fact
+        current_fact += 1
+        if current_fact >= len(fact_order):
+            current_fact = 0
+
+    return __facts.get(fact_order[number]).upper()
+
 
