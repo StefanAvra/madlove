c0077cb0f3c7a030cb6c12a43dffd3a13060be7b
stefanavra <stefan.avra@gmail.com>
2019-06-14 22:23:58 +0200

    added credits

--- string_resource.py
+++ string_resource.py
@@ -14,16 +14,16 @@ __strings = {
 }
 
 __facts = {
-    0: 'Smoking clogs the arteries and causes heart attacks and strokes.',
-    1: 'Smoking can cause a slow and painful death.',
+    0: 'Smoking clogs the arteries\nand causes heart attacks\nand strokes.',
+    1: 'Smoking can cause a slow\nand painful death.',
     2: 'Smoking kills.',
-    3: 'Smoking causes lung, oral and laryngeal cancer.',
+    3: 'Smoking causes lung, oral\nand laryngeal cancer.',
     4: 'Smoking causes heart disease.',
-    5: 'At least four of the actors who played the Marlboro Man have died of smoking-related diseases.',
-    6: '67% of Indonesia\'s male popularity smokes.',
-    7: 'The Marlboro Man is still used in Japan, where smoking is widespread in the male population.',
-    8: 'Philip Morris (the company that sells Marlboro) is known to sponsor legal costs in lawsuits, if a country'
-       ' happens to sue another country over anti-smoking laws.'
+    5: 'At least four of the actors\nwho played the iconic MadLove\nMan have died of\nsmoking-related diseases.',
+    6: '67% of Indonesia\'s\nmale population smokes.',
+    7: 'The MadLove Man is still used\nin Japan, where smoking is\nwidespread in the male\npopulation.',
+    8: 'The company that sells MadLove\ntobacco is known to sponsor legal\ncosts in lawsuits, if a country\n'
+       ' happens to sue another country\nover anti-smoking laws.'
 }
 
 __combos = {
@@ -31,6 +31,34 @@ __combos = {
     50: 'ultra combo!',
     100: 'holy moly!'
 }
+
+__credit_views = {
+    0: "A project by\nGurkiman\n&\nStefan 'Avra' Avramescu",
+    1: """
+Game Idea by Gurkiman
+
+Game Design by
+Gurkiman and Avra
+
+Programmed by
+Avra
+Code published under MIT License
+
+Graphic Designer
+Gurkiman
+""",
+    2: """
+Music by Ozzed
+
+in-game Font
+© 2012 The Press Start 2P Project Authors (cody@zone38.net),
+with Reserved Font Name "Press Start 2P".
+""",
+    3: """
+© 2019 Gurkiman, Avra
+"""
+}
+
 
 fact_order = [i for i in range(len(__facts) - 1)]
 random.shuffle(fact_order)
@@ -48,9 +76,14 @@ def get_fact(number=None):
         current_fact += 1
         if current_fact >= len(fact_order):
             current_fact = 0
-
     return __facts.get(fact_order[number]).upper()
 
 
 def get_combo_msg(multi):
     return __combos.get(multi)
+
+
+def get_credits(view_no):
+    # todo: formatting
+    return __credit_views.get(view_no)
+
